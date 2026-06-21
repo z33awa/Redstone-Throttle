b02f2b3522c68ed600f10364f670a28050adeac3
z33awa <78519467+z33awa@users.noreply.github.com>
2026-06-21 20:53:05 +0800

    Update RedstoneSpeedModulatorBlockEntity.java

--- src/main/java/com/z33awa/redstonethrottle/content/modulator/RedstoneSpeedModulatorBlockEntity.java
+++ src/main/java/com/z33awa/redstonethrottle/content/modulator/RedstoneSpeedModulatorBlockEntity.java
@@ -174,12 +174,12 @@ public class RedstoneSpeedModulatorBlockEntity extends GeneratingKineticBlockEnt
      */
     public static Direction redstoneLeft(Direction facing) {
         return switch (facing) {
-            case NORTH -> Direction.EAST;
-            case SOUTH -> Direction.EAST;
-            case EAST -> Direction.NORTH;
-            case WEST -> Direction.NORTH;
-            case UP -> Direction.WEST;
-            case DOWN -> Direction.EAST;
+            case NORTH -> Direction.WEST;
+            case SOUTH -> Direction.WEST;
+            case EAST -> Direction.SOUTH;
+            case WEST -> Direction.SOUTH;
+            case UP -> Direction.EAST;
+            case DOWN -> Direction.WEST;
         };
     }
 
@@ -189,12 +189,12 @@ public class RedstoneSpeedModulatorBlockEntity extends GeneratingKineticBlockEnt
      */
     public static Direction redstoneRight(Direction facing) {
         return switch (facing) {
-            case NORTH -> Direction.WEST;
-            case SOUTH -> Direction.WEST;
-            case EAST -> Direction.SOUTH;
-            case WEST -> Direction.SOUTH;
-            case UP -> Direction.EAST;
-            case DOWN -> Direction.WEST;
+            case NORTH -> Direction.EAST;
+            case SOUTH -> Direction.EAST;
+            case EAST -> Direction.NORTH;
+            case WEST -> Direction.NORTH;
+            case UP -> Direction.WEST;
+            case DOWN -> Direction.EAST;
         };
     }
 
